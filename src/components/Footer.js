2efeb89fbca04ef8610d4f66e74ae5d8b12baed3
Gustavo iglesias <ggihoff@gmail.com>
2020-04-07 15:50:55 -0300

    first commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer>
-           <p>Gustavo iglesias</p>
+           <strong>Develop by Gustavo iglesias</strong>
            <ul class="left-social">
-      <li><i class="fab fa-github"></i></li>
+           <a target="_blank" href="https://github.com/ggihoff"><li><i class="fab fa-github"></i></li></a>
       <a target="_blank" href="https://www.linkedin.com/in/gustavo-iglesias-9b2ba8193/"><li><i class="fab fa-linkedin-in"></i></li></a>
     </ul>
         </footer>
